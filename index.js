2e7d3053baae906af55c73b2209ba6210a5ccb94
Manan-dev <mananpatel6902@gmail.com>
2021-12-26 20:04:13 -0500

    added some styling

--- index.js
+++ index.js
@@ -1,60 +1,70 @@
 const url = 'https://api.github.com/users/';
-const input = document.getElementById('text');
 const searchBtn = document.getElementById('search');
-const name = document.getElementById('name');
-const imag = document.getElementById('image');
-const bio = document.getElementById('bio');
-const join_date = document.getElementById('join_date');
-const repos = document.getElementById('repos');
-const followers = document.getElementById('followers');
-const following = document.getElementById('following');
-const Location = document.getElementById('location');
-const link = document.getElementById('link');
-const company = document.getElementById('company');
+const input = document.getElementById('text');
 
 const getUser = async (user) => {
 	try {
 		let data = await fetch(url + user);
-		let res = await data.json();
-		image.src = res.avatar_url;
-		name.innerText = res.name;
-		bio.innerText = res.bio;
-		let [year, month, date] = res.created_at.split('-');
-		date = res.created_at.substring(8, 10);
-		const months = [
-			'Jan',
-			'Feb',
-			'Mar',
-			'Apr',
-			'May',
-			'Jun',
-			'Jul',
-			'Aug',
-			'Sep',
-			'Oct',
-			'Nov',
-			'Dec',
-		];
-		month = months[month - 1];
-		join_date.innerText = `Joined ${date} ${month} ${year}`;
-		bio.innerText = res.bio;
-		repos.innerText = `Repos:  ${res.public_repos}`;
-		followers.innerText = `Followers:  ${res.followers}`;
-		following.innerText = `Following: ${res.following}`;
-		Location.innerText = `Location: ${res.location}`;
-		link.innerText = res.blog;
-		link.href = res.blog;
-		company.innerText = `Company: ${res.company}`;
+		let response = await data.json();
+		changeTimeFormat(response);
+		changeCard(response);
 	} catch (error) {
 		console.log(error.message);
 	}
 };
 
-// getUser('manan-dev');
+function changeTimeFormat(time) {
+	let [year, month, date] = time.created_at.split('-');
+	date = time.created_at.substring(8, 10);
+	const months = [
+		'Jan',
+		'Feb',
+		'Mar',
+		'Apr',
+		'May',
+		'Jun',
+		'Jul',
+		'Aug',
+		'Sep',
+		'Oct',
+		'Nov',
+		'Dec',
+	];
+	month = months[month - 1];
+	return [date, month, year];
+}
+const changeCard = (user) => {
+	const card = `
+	<div class="card">
+		<div>
+			<img class="image" src=${user.avatar_url} alt=${user.name} />
+		</div>
+		<div class="main">
+			<h2>${user.name}</h2>
+			<h5 class="join_date">${changeTimeFormat(user).join(' ')}</h5>
+			<p>${user.bio}</p>
+			<ul class="info">
+				<li>Repos </br>${user.public_repos}</li>
+				<li>Followers </br>${user.followers}</li>
+				<li>Following </br>${user.following}</li>
+			</ul>
+			<ul class="contact_details">
+				<li>Location: ${user.location}</li>
+				<a href="${user.blog}" target="_blank">${user.blog}</a>
+				<li>${user.company}</li>
+			</ul>
+		</div>
+	</div>
+	`;
+	main.innerHTML = card;
+};
 
-searchBtn.addEventListener('click', (event) => {
+searchBtn.addEventListener('click', () => {
 	getUser(input.value);
+	changeCard(input.value);
 	// getUser('manan-dev');
+	// changeCard('manan-dev');
+
 	// getUser('jaredthecoder');
 	// getUser('florinpop17');
 });
